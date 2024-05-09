c7b00636854bf08dea177459c78c966898f7888e
Maks <maksmunait@gmail.com>
2024-05-10 01:47:48 +0200

    enums

--- second_month/src/main.rs
+++ second_month/src/main.rs
@@ -1,55 +1,92 @@
-#[derive(Debug)]
 
+/* enums */
 
-struct Rectangle {
-    width: u32,
-    height: u32,
+struct QuitMessage; // unit struct
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); // tuple struct
+struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
+
+impl Message {
+    fn call(&self) {
     }
+}
+
+enum Option<T> {
+    None,
+    Some(T),
+}
+
+
+fn main(){
+
+    let m: Message = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    // let absent_number: Option<i32> = None;
 
     /*
-    fn somme(&self) -> u32 {
-        self.width * self.height
+    enum IpAddrKind {
+        V4,
+        V6,
     }
-    */
-}
-
-
-fn main() {
-    //let width1: u32 = 22;
-    //let height1: u32 = 22;
-    // let rect: (u32, u32) = (22, 22);
-
-    let scale: u32 = 4;
-    let rect_un: Rectangle = Rectangle {width: 22, height: 22};
-    let rect_deux: Rectangle = Rectangle {
-        width: dbg!(50 * scale),
-        height: 131
-    };
     
-    // println!("The area of the rectangle is {} square pixels.", area(rect));
-    println!("The area of the rectangle is {} square pixels.", area(&rect_un));
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
 
-    // println!("rect_deux is {:?}", rect_deux);
-    // println!("rect_deux is {:#?}", rect_deux);
-    dbg!(&rect_deux);
-
-    let rect_trois: Rectangle = Rectangle{
-        width: 64, height: 29
+    let home: IpAddr = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
     };
 
-    println!("rect_trois is {}", rect_trois.area());
+    let loopback: IpAddr = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+    */
+
+
+    // Continue
+    /*
+    enum IpAddr{
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+    let home: IpAddr = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback: IpAddr = IpAddr::V6(String::from("::1"));
+    */
+
+
+    /*
+    struct Ipv4Addr {
+        // --snip--
+    }
+    
+    struct Ipv6Addr {
+        // --snip--
+    }
+    
+    enum IpAddr {
+        V4(Ipv4Addr),
+        V6(Ipv6Addr),
+    }
+     */
+
 }
 
-
-fn area(rectangle: &Rectangle) -> u32{
-    rectangle.width * rectangle.height
-}
-
-// fn area(dimensions: (u32, u32)) -> u32 {
-//     dimensions.0 * dimensions.1
-// }
---
+++ second_month/src/structs.rs
@@ -0,0 +1,83 @@
+#[derive(Debug)]
+
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn somme(&self) -> u32 {
+        self.width + self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    /* Rectangle rather than having
+        to specify the same value twice */
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size
+        }
+    }
+}
+
+
+fn main() {
+    //let width1: u32 = 22;
+    //let height1: u32 = 22;
+    // let rect: (u32, u32) = (22, 22);
+
+    let scale: u32 = 4;
+    let rect_un: Rectangle = Rectangle {width: 22, height: 22};
+    let rect_deux: Rectangle = Rectangle {
+        width: dbg!(50 * scale),
+        height: 131
+    };
+    
+    // println!("The area of the rectangle is {} square pixels.", area(rect));
+    println!("The area of the rectangle is {} square pixels.", area(&rect_un));
+
+    // println!("rect_deux is {:?}", rect_deux);
+    // println!("rect_deux is {:#?}", rect_deux);
+    dbg!(&rect_deux);
+
+    let rect_trois: Rectangle = Rectangle{
+        width: 64, height: 29
+    };
+
+    println!("rect_trois is {}", rect_trois.area());
+    println!("somme: {} and {} is {}", rect_trois.width, rect_trois.height ,rect_trois.somme());
+
+    if rect_trois.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect_trois.width);
+    }
+
+    println!("rect_deux can hold rect_trois: {}", rect_deux.can_hold(&rect_trois));
+    println!("rect_un can hold rect_deux: {}", rect_un.can_hold(&rect_deux));
+
+    let square_of_rect: Rectangle = Rectangle::square(4);
+    
+    dbg!(&square_of_rect);
+
+}
+
+
+fn area(rectangle: &Rectangle) -> u32{
+    rectangle.width * rectangle.height
+}
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
